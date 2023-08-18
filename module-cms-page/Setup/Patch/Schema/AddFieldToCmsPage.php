61fb7f7c0e264896eb430ef035a00bc6cc7c300c
pawan <pawan.parmar1@gmail.com>
2023-08-18 08:17:54 +0530

    added cms module

---
+++ module-cms-page/Setup/Patch/Schema/AddFieldToCmsPage.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Pawan\CmsPage\Setup\Patch\Schema;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+
+/**
+* Patch is mechanism, that allows to do atomic upgrade data changes
+*/
+class AddFieldToCmsPage implements SchemaPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface $moduleDataSetup
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->startSetup();
+        $this->moduleDataSetup->getConnection()->addColumn(
+            $this->moduleDataSetup->getTable('cms_page'),
+            'custom',
+            [
+               'type' => Table::TYPE_SMALLINT,
+               'nullable' => false,
+               'comment'  => 'custom',
+            ]
+        );
+        $this->moduleDataSetup->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
