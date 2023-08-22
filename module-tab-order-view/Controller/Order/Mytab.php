5431671c6f1f4bf93ca903cd7fac8b4c7147a404
pawan <pawan.parmar1@gmail.com>
2023-08-22 08:10:46 +0530

    added tab module

---
+++ module-tab-order-view/Controller/Order/Mytab.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pawan\TabOrderView\Controller\Order;
+
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Sales\Controller\OrderInterface;
+
+class Mytab extends \Magento\Sales\Controller\AbstractController\View implements OrderInterface, HttpGetActionInterface
+{
+    
+}
